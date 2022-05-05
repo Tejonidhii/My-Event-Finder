78a1fe4c9aee348f908f4d5497b655d5957f54aa
Tejo <tejonidhii@gmail.com>
2022-05-05 14:11:36 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Routes from "./Routes";
+import ReduxToastr from "react-redux-toastr";
+
+
+function App() {
+  return (
+    <>
+      <Routes />
+      
+    </>
+  );
+}
+
+export default App;
